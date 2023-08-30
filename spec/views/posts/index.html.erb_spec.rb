ca1e5905fe262b0481dbb74ead5c3cea3b3965c0
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-08-30 04:58:18 -0500

    Add User and Post models

---
+++ spec/views/posts/index.html.erb_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "posts/index", type: :view do
+  before(:each) do
+    assign(:posts, [
+      Post.create!(
+        title: "Title",
+        body: "MyText",
+        user: nil,
+        views: 2
+      ),
+      Post.create!(
+        title: "Title",
+        body: "MyText",
+        user: nil,
+        views: 2
+      )
+    ])
+  end
+
+  it "renders a list of posts" do
+    render
+    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+  end
+end
