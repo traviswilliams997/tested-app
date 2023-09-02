2cb93f422b2489414a7486db04f3e18fb8078fb9
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-01 20:43:30 -0500

    Edit index spec

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "posts/index", type: :view do
+  current_user = User.first_or_create!(email: 'travis@example.com', password: 'password', password_confirmation: 'password')
+
   before(:each) do
     assign(:posts, [
       Post.create!(
         title: "Title",
-        body: "MyText",
-        user: nil,
-        views: 2
+        body: "MyNewText",
+        user: current_user,
+        views: 14
       ),
       Post.create!(
         title: "Title",
-        body: "MyText",
-        user: nil,
-        views: 2
+        body: "MyNewText",
+        user: current_user,
+        views: 12
       )
     ])
   end
@@ -22,8 +24,9 @@ RSpec.describe "posts/index", type: :view do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("MyNewText".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(current_user.id.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(14.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new(12.to_s), count: 1
   end
 end
